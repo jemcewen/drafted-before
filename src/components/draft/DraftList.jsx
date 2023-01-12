1348623f6d2958b82b3f44a33f46b370ba30e327
jamesmcewen <jamesmcewen5@gmail.com>
2023-01-12 13:47:16 -0800

    Add pagination to draft list

--- src/components/draft/DraftList.jsx
+++ src/components/draft/DraftList.jsx
@@ -1,46 +1,74 @@
 import { useState } from 'react';
+import Pagination from '../layout/Pagination';
 import DraftPick from './DraftPick';
 import PropTypes from 'prop-types';
 
 function DraftList({ draftAthletes, position }) {
   const [filter, setFilter] = useState(false);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [athletesPerPage] = useState(12);
+
+  const indexOfLastAthlete = currentPage * athletesPerPage;
+  const indexOfFirstAthlete = indexOfLastAthlete - athletesPerPage;
+  const currentAthletes = draftAthletes.slice(
+    indexOfFirstAthlete,
+    indexOfLastAthlete
+  );
+
+  const nextPage = () => setCurrentPage((prev) => prev + 1);
+  const previousPage = () => setCurrentPage((prev) => prev - 1);
+
   const handleChange = () => {
     setFilter(!filter);
   };
 
   return (
-    <div className='rounded-lg shadow-lg card bg-base-100'>
-      <div className='card-body'>
-        <div className='flex items-center justify-between'>
-          <h2 className='text-3xl my-4 font-bold card-title'>Draft Order</h2>
-          <div>
-            <div class='form-control w-36'>
-              <label class='cursor-pointer label'>
-                <span class='label-text'>Filter Position</span>
-                <input
-                  type='checkbox'
-                  class='toggle'
-                  checked={filter}
-                  onChange={handleChange}
-                />
-              </label>
+    <>
+      <div className='rounded-lg shadow-lg card bg-base-100'>
+        <div className='card-body'>
+          <div className='flex items-center justify-between'>
+            <h2 className='text-3xl my-4 font-bold card-title'>Draft Order</h2>
+            <div>
+              <div class='form-control w-36'>
+                <label class='cursor-pointer label'>
+                  <span class='label-text'>Filter Position</span>
+                  <input
+                    type='checkbox'
+                    class='toggle'
+                    checked={filter}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
             </div>
           </div>
-        </div>
-        {filter
-          ? draftAthletes
-              ?.filter((athlete) => {
-                return athlete.position.name === position;
-              })
-              .map((athlete) => (
+          {filter
+            ? draftAthletes
+                ?.filter((athlete) => {
+                  return athlete.position.name === position;
+                })
+                .map((athlete) => (
+                  <DraftPick key={athlete.id} athlete={athlete} />
+                ))
+            : currentAthletes?.map((athlete) => (
                 <DraftPick key={athlete.id} athlete={athlete} />
-              ))
-          : draftAthletes?.map((athlete) => (
-              <DraftPick key={athlete.id} athlete={athlete} />
-            ))}
+              ))}
+        </div>
       </div>
-    </div>
+      {!filter && (
+        <div className='mt-6'>
+          <Pagination
+            athletesPerPage={athletesPerPage}
+            totalAthletes={draftAthletes.length}
+            indexOfFirstAthlete={indexOfFirstAthlete}
+            indexOfLastAthlete={indexOfLastAthlete}
+            nextPage={nextPage}
+            previousPage={previousPage}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,8 +6,8 @@ function About() {
         <strong>One hundred and ninety eight</strong> players were drafted
         before NFL quarterback Tom Brady in the year 2000. Over twenty years and
         seven Super Bowl wins later he is now the undisputed greatest to every
-        play the game of football. This web application lets you search for NFL
-        players and see who was selected before them.
+        play the game of football. This simple React web application lets you
+        search for NFL players and see who was selected before them.
       </p>
       <p className='text-lg text-gray-400'>
         Created by <strong>James McEwen</strong>
