1ff016a56f37ef5f00361f0c72113299dea6e459
jamesmcewen <jamesmcewen5@gmail.com>
2023-01-15 14:33:51 -0800

    Fix typo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
       <p className='mb-8 text-2xl font-light'>
         <strong>One hundred and ninety eight</strong> players were drafted
         before NFL quarterback Tom Brady in the year 2000. Over twenty years and
-        seven Super Bowl wins later he is now the undisputed greatest to every
+        seven Super Bowl wins later he is now the undisputed greatest to ever
         play the game of football. This simple React web application lets you
         search for NFL players and see who was selected before them.
       </p>
